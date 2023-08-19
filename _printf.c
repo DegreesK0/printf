a54e5c3ff47fbead1c422c26af4f7c464e0cc375
DegreesK0 <kelvinbora@gmail.com>
2023-08-19 19:25:01 +0000

    "Added a testing folder, main.h and our _printf.c. Starting on making _printf.c print simple strings"

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - prints out a string
+ * format - the character to be printed out.
+ *
+ * Return - the number of characters printed, excluding the null byte
+ */
+
+
+int _printf(const char *format, ...)
+{
+
+}
