c3f93a22502593ee9237680da04f10bfb0e810d0
DegreesK0 <kelvinbora@gmail.com>
2023-08-19 19:33:02 +0000

    test

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _printf - prints out a string
@@ -10,5 +11,18 @@
 
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int format_len = 0;
 
+	while (!format[format_len])
+		format_len++;
+
+
+	for (i = 0; i <)
+	{
+
+	}
+
+	printf("format_len:")
+	return (format_len++)
 }
