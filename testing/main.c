06271bc9f650863ecc1615ac06879fd3d5fdfeef
Pcosby5 <pcosby50@gmail.com>
2023-08-20 21:05:26 +0000

    testing edge cases

--- testing/main.c
+++ testing/main.c
@@ -25,14 +25,12 @@ int main(void)
 	/* /1* _printf("three %%%%% "); *1/ */
 	/* _printf("two %%% sign", ch); */
 	/* _printf("number: %d", num); */
-
+	_printf(NULL);
 
 	/* int len; */
 	/* int len2; */
 	/* unsigned int ui; */
 	/* void *addr; */
-
-
 
 	/* len = _printf("Let's try to printf a simple sentence.\n"); */
 	/* len2 = printf("Let's try to printf a simple sentence.\n"); */
@@ -54,8 +52,8 @@ int main(void)
 	printf("String:[%s]\n", "I am a string !");
 	/* _printf("Address:[%p]\n", addr); */
 	/* printf("Address:[%p]\n", addr); */
-	/*len = */_printf("Percent:[%%]\n");
-	/*len2 = */printf("Percent:[%%]\n");
+	/*len = */ _printf("Percent:[%%]\n");
+	/*len2 = */ printf("Percent:[%%]\n");
 	/* _printf("Len:[%d]\n", len); */
 	/* printf("Len:[%d]\n", len2); */
 	/* _printf("Unknown:[%r]\n"); */
