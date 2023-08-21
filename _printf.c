dacf295612d092a4328350cb54a129b038b3d0b1
DegreesK0 <kelvinbora@gmail.com>
2023-08-21 22:24:18 +0000

    Reverted previous changes. Tried a second time to add select_con_spec.c

--- _printf.c
+++ _printf.c
@@ -12,10 +12,16 @@
 
 int _printf(const char *format, ...)
 {
-	 con_specs_t cons[] = {
-		{'c', con_char}, {'s', con_str}, {'%', con_perc}, {' ', con_space},
-		{'d', con_dec}, {'i', con_int}, {'\0', con_space}};
-	int index = 0, chars_printed = 0, j = 0, temp = 0;
+	 /* con_specs_t cons[] = { */
+		/* {'c', con_char}, */
+		/* {'s', con_str}, */
+		/* {'%', con_perc}, */
+		/* {' ', con_space}, */
+		/* {'d', con_dec}, */
+		/* {'i', con_int}, */
+		/* {'\0', con_space}}; */
+
+	int index = 0, chars_printed = 0; /*j = 0*/
 	va_list args;
 
 	va_start(args, format);
@@ -25,23 +31,17 @@ int _printf(const char *format, ...)
 	{
 		if (format[index] == '%')
 		{
-			if (format[index + 1] == '\0')
+			index++;
+			if (format[index] == '\0')
 				return (-1);
-			temp = index; /*index++;*/
-			/* select_con_spec(format, index, args); */
-			for (j = 0; cons[j].spec != '\0'; j++)
-			{
-				if (format[index + 1] == cons[j].spec)
-				{
-					chars_printed += cons[j].func(args);
-					index++;
-				}
-			}
-			if (temp == index)
-			{
-				our_putchar(format[index]);
-				chars_printed++;
-			}
+			chars_printed += select_con_spec(format, index, args);
+			/* for (j = 0; cons[j].spec != '\0'; j++) */
+			/* { */
+			/*	if (format[index] == cons[j].spec) */
+			/*	{ */
+			/*		chars_printed += cons[j].func(args); */
+			/*	} */
+			/* } */
 		}
 		else
 		{
@@ -49,6 +49,7 @@ int _printf(const char *format, ...)
 			chars_printed++;
 		}
 	}
-	va_end(args); /* printf("chars printed: %d\n", chars_printed); */
+	va_end(args);
+	/* printf("chars printed: %d\n", chars_printed); */
 	return (chars_printed);
 }
