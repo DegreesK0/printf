d8066b784fae0c265d4756a0696d12ea516e2b41
DegreesK0 <kelvinbora@gmail.com>
2023-08-21 16:10:14 +0000

    Testing something

--- testing/_printf.c
+++ testing/_printf.c
@@ -13,10 +13,13 @@
 int _printf(const char *format, ...)
 {
 	con_specs_t cons[] = {
-		{'c', con_char}, {'s', con_str}, {'%', con_perc}, {' ', con_space},
-		{'\0', con_space}
-	};
-	int i = 0, j = 0, chars_printed = 0/*, temp = 0*/;
+		{'c', con_char},
+		{'s', con_str},
+		{'%', con_perc},
+		{' ', con_space},
+		{'\0', con_space}};
+
+	int i = 0, j = 0, chars_printed = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -24,32 +27,26 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
-		{
-			i++; /*temp = i;*/
-			/* printf("<i = %d, format[i] = %c>\n", i, format[i]); */
+		if (format[i] == '%')
+		{ /* printf("<i = %d, format[i] = %c>\n", i, format[i]); */
+			i++;
 			for (j = 0; cons[j].spec != '\0'; j++)
 			{ /* if (strcmp(&format[i], cons[j].spec) == 0) */
-				if (format[i + 1] == cons[j].spec)
+				if (format[i] == cons[j].spec)
 				{
 					chars_printed += cons[j].func(args);
-					/* i++; */
 					/* if (format[i] == '%' && format[i + 1] == '%') */ /*	i++; */
 				}
 			}
-			/* if (temp == i) */
-			/* { */
-			/*	our_putchar(format[i]); */
-			/*	chars_printed++; */
-			/* } */
 		}
 		else
 		{
-			our_putchar(format[i]);
+			write(1, &format[i], 1);
 			chars_printed++;
 		}
 	}
 	va_end(args);
-	/* printf("RETURN VALUE: %d\n", chars_printed); */
+	printf("chars printed: %d\n", chars_printed);
 	return (chars_printed);
 }
+
