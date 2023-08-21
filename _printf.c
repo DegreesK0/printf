c0cce511bdf2d3eb921cedd13a7fd8ac232cb8c7
DegreesK0 <kelvinbora@gmail.com>
2023-08-21 15:34:15 +0000

    Should fix the % errors

--- _printf.c
+++ _printf.c
@@ -13,13 +13,10 @@
 int _printf(const char *format, ...)
 {
 	con_specs_t cons[] = {
-		{'c', con_char},
-		{'s', con_str},
-		{'%', con_perc},
-		{' ', con_space},
-		{'\0', con_space}};
-
-	int i = 0, j = 0, chars_printed = 0;
+		{'c', con_char}, {'s', con_str}, {'%', con_perc}, {' ', con_space},
+		{'\0', con_space}
+	};
+	int i = 0, j = 0, chars_printed = 0, temp = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -27,25 +24,32 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
-		{ /* printf("<i = %d, format[i] = %c>\n", i, format[i]); */
-			i++;
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			temp = i;
+			/* printf("<i = %d, format[i] = %c>\n", i, format[i]); */
 			for (j = 0; cons[j].spec != '\0'; j++)
 			{ /* if (strcmp(&format[i], cons[j].spec) == 0) */
-				if (format[i] == cons[j].spec)
+				if (format[i + 1] == cons[j].spec)
 				{
 					chars_printed += cons[j].func(args);
+					i++;
 					/* if (format[i] == '%' && format[i + 1] == '%') */ /*	i++; */
 				}
+			}
+			if (temp == i)
+			{
+				our_putchar(format[i]);
+				chars_printed++;
 			}
 		}
 		else
 		{
-			write(1, &format[i], 1);
+			our_putchar(format[i]);
 			chars_printed++;
 		}
 	}
 	va_end(args);
-	/* printf("\n"); */ /* printf("chars printed: %d\n", chars_printed); */
+	/* printf("RETURN VALUE: %d\n", chars_printed); */
 	return (chars_printed);
 }
